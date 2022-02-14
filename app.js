84486ed0c15e172cb59f21ff607fa13a751d41ac
Ashwani Kumar Singh <dev.ak.singh@outlook.com>
2022-02-14 12:24:54 +0530

    passed content to home.ejs

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+// Global post array
+const posts = [];
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,7 +21,8 @@ app.use(express.static("public"));
 
 app.get('/', (req, res) => {
   res.render('home', {
-    homeStartingContent
+    homeStartingContent,
+    posts
   })
 })
 
@@ -39,8 +43,12 @@ app.get('/compose', (req, res) => {
 })
 
 app.post('/compose', (req, res) => {
-  const post = req.body
-  console.log(post)
+  const post = {
+    title: req.body.newTitle,
+    post: req.body.newPost
+  }
+  posts.push(post)
+  res.redirect('/')
 })
 
 app.listen(3000, function () {
